038c7beec38973ea3da8d5ce13f9c2794d6f8078
Elliott <elliotthamer@gmail.com>
2024-10-18 10:15:39 +0100

    recursive bitwise or

---
+++ _testing_functions.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+def print_columns(cols: list[Any], width: int = 20) -> None:
+    # center aligned
+    col_width = width
+    out_str = ""
+
+    for col in cols:
+        col = str(col)
+        out_str += f"{col:^{col_width}} | "
+
+    print(out_str[:-3])
+
+def print_first_row(cols: list[Any], width: int = 20) -> None:
+    # center aligned
+    col_width = width
+    out_str = ""
+
+    for col in cols:
+        col = str(col)
+        out_str += f"{col:^{col_width}} | "
+
+    print(out_str[:-3])
+    print("-"*(width*len(cols)))
