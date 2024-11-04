3ab7d3a51086eaf6eae7a1accaa4c734efdb89c8
Elliott <elliotthamer@gmail.com>
2024-11-04 08:31:04 +0000

    string manip

---
+++ rotating_string_796.py
@@ -0,0 +1,15 @@
+# rotating_string_796
+# https://leetcode.com/problems/rotating-string
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+
+        
+        for _ in range(len(s)):
+            if s == goal:
+                return True
+            s = s[1:] + s[0]
+            
+        return False
