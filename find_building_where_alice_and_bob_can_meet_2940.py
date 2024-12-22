8730fabb116ce8976605e86f5bb4a40fff928413
Elliott <elliotthamer@gmail.com>
2024-12-22 09:27:49 +0000

    min heap and bookkeeping

---
+++ find_building_where_alice_and_bob_can_meet_2940.py
@@ -0,0 +1,35 @@
+# find_building_where_alice_and_bob_can_meet_2940
+# https://leetcode.com/problems/find-building-where-alice-and-bob-can-meet/description/?envType=daily-question&envId=2024-12-22
+
+
+from collections import defaultdict
+import heapq
+
+class Solution:
+    def leftmostBuildingQueries(self, heights: list[int], queries: list[list[int]]) -> list[int]:
+
+        res = [-1] * len(queries)
+
+        groups = defaultdict(list) # [(required height, query index)]
+
+        for i, q in enumerate(queries):
+            l, r = sorted(q)
+
+            if l == r or heights[r] > heights[l]:
+                res[i] = r
+            else:
+                h = max(heights[l],  heights[r])
+                groups[r].append((h, i))
+
+        min_heap = []
+
+        for i, h in enumerate(heights):
+            for q_h, q_i in groups[i]:
+                heapq.heappush(min_heap, (q_h, q_i))
+
+            while min_heap and h > min_heap[0][0]:
+                q_h, q_i = heapq.heappop(min_heap)
+                res[q_i] = i
+
+        return res
+        
