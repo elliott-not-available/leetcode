6d9f363444f9f9f719952c900a63bd72df3c5fa7
Elliott <elliotthamer@gmail.com>
2025-01-29 09:40:41 +0000

    was not paying attention... (union by rank and path compression?)

---
+++ redundant_connection_684.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/redundant-connection/description/?envType=daily-question&envId=2025-01-29
+
+class Solution:
+    def findRedundantConnection(self, edges: list[list[int]]) -> list[int]:
+
+        # union by rank
+        # path compresion
+
+        N = len(edges)
+        par = [i for i in range(N+1)] * N # ith node -> parent (1 - n)
+        rank = [1] * (N+1)
+
+        def find(n):
+            if n != par[n]:
+                par[n] = find(par[n])
+            return par[n]
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return False
+            
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+
+            return True
+
+        for n1, n2 in edges:
+            if not union(n1, n2):
+                return [n1, n2]
