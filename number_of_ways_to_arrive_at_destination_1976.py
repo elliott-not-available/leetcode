a415659804702f5bf082f780109ae94487c6f66c
Elliott <elliotthamer@gmail.com>
2025-03-23 11:17:16 +0000

    minheap (djikstras) - medium

---
+++ number_of_ways_to_arrive_at_destination_1976.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/number-of-ways-to-arrive-at-destination/description/?envType=daily-question&envId=2025-03-23
+
+class Solution:
+    def countPaths(self, n: int, roads: list[list[int]]) -> int:
+
+        return
+        
