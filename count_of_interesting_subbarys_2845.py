84fbc7701600188086bdb4b764c5cc017ef685ae
Elliott <elliotthamer@gmail.com>
2025-04-25 09:47:27 +0100

    hashmap prefixsum modulo - medium (used editorial still dont understand) very mathsy i guess?

--- count_of_interesting_subbarys_2845.py
+++ count_of_interesting_subbarys_2845.py
@@ -1,15 +1,39 @@
 # https://leetcode.com/problems/count-of-interesting-subarrays/description/?envType=daily-question&envId=2025-04-25
-
+from collections import defaultdict
 class Solution:
     def countInterestingSubarrays(self, nums: list[int], modulo: int, k: int) -> int:
         N = len(nums)
-        for l in range(N):
-            for r in range(l, N):
-                cur = nums[l:r]
+        # count = [0] * N
+        # cur_cnt = 0
+        # for i in range(nums):
+        #     if nums[i] % modulo == k:
+        #         cur_cnt += 1
 
-                # for i in range segment check nums[i] % mod == k, if so cnt +1
-                # now check cnt % mod == k
+        #     count[i] = cur_cnt
 
 
-        return
+
+
+        # return
+        hm = defaultdict(int)
+        res = 0
+        cur = 0
+
+        for i in range(N):
+
+            if nums[i] % modulo == k:
+                cur += 1
+                cur = cur % modulo
+
+            # dont understand this bit
+            if cur == k:
+                res += 1
+
+            res += hm[(cur - k) % modulo]
+            hm[cur] += 1
+            
+            # abc = (cur - k + modulo) % modulo
+            # res += cnt[abc]
+            # cnt[cur % modulo] += 1
+        return res
         
