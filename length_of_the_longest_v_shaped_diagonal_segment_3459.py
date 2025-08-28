20fad4ad2dca3b16696f8e346052284e1439ea8f
Elliott <elliotthamer@gmail.com>
2025-08-28 08:07:36 +0100

    skipped

---
+++ length_of_the_longest_v_shaped_diagonal_segment_3459.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/length-of-longest-v-shaped-diagonal-segment/?envType=daily-question&envId=2025-08-27
+from typing import List
+
+class Solution:
+    def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
+        return
