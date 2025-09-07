32e91dd8b1579def9edac7695d807a6062b55ab2
Elliott <elliotthamer@gmail.com>
2025-09-07 09:25:13 +0100

    forgot

---
+++ minimum_operations_to_make_array_elements_zero_3495.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/minimum-operations-to-make-array-elements-zero/description/?envType=daily-question&envId=2025-09-06
+
+class Solution:
+    def minOperations(self, queries: list[list[int]]) -> int:
+
+        # operation = floor(a  / 4)
+
+        # for each q in query: reduce each q[0] to q[1] to 0 with operation
+
+        # start with brute force, probably not needed
+
+        return
+        
