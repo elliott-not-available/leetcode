47f70f78485292c25315482e8e0062ab6583e2e4
Elliott <elliotthamer@gmail.com>
2025-10-22 10:49:04 +0100

    binary, 2pntr - hard

---
+++ maximum_frequency_of_an_element_after_performing_operations_2_3347.py
@@ -0,0 +1,40 @@
+#https://leetcode.com/problems/maximum-frequency-of-an-element-after-performing-operations-ii/description/?envType=daily-question&envId=2025-10-22
+from collections import Counter
+from bisect import bisect_left, bisect_right
+class Solution:
+    def maxFrequency(self, nums: list[int], k: int, numOperations: int) -> int:
+
+        nums.sort()
+
+        def max_freq_of_array(self, nums, k, numOperatrions):
+            count = Counter(nums)
+
+            res = 0
+
+            for v in count.keys():
+                l = bisect_left(nums, v-k)
+                r = bisect_right(nums, v+k) - 1
+
+                freq = min(r-l+1, numOperations + count[v])
+                res = max(res, freq)
+
+            return res
+        
+        arr_max_freq = max_freq_of_array(nums, k, numOperations)
+
+        left = 0
+        other = 0
+
+        for right in range(len(nums)):
+
+            while nums[right] > nums[left] + 2*k:
+                left += 1
+            
+            other = max(other, right-left + 1)
+
+            if other >= numOperations:
+                other = numOperations
+                break
+
+        return max(arr_max_freq, other)
+        
