b1a4383ae3bcd42191e9b44c61e846127c661b21
Elliott <elliotthamer@gmail.com>
2025-12-03 08:28:25 +0000

    skipped

---
+++ count_number_of_trapezoids_2_3625.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/count-number-of-trapezoids-ii/description/?envType=daily-question&envId=2025-12-03
+
+class Solution:
+    def countTrapezoids(self, points: list[list[int]]) -> int:
+
+        return
+        
