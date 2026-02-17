f2a15bee38d77f0155488cce54f2de67df68c6a9
Elliott <elliotthamer@gmail.com>
2026-02-17 08:51:56 +0000

    bit repr - easy

---
+++ binary_watch_401.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/binary-watch/?envType=daily-question&envId=2026-02-17
+
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> list[str]:
+        res = []
+
+        for h in range(12):
+            for m in range(60):
+                if bin(h).count("1") + bin(m).count("1") == turnedOn:
+                    res.append(f"{h}:{m:02d}")
+        return res
