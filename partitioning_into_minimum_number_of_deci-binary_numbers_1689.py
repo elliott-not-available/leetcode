acb0eb849ac59e4c70167eddb8c75999c1771018
Elliott <elliotthamer@gmail.com>
2026-03-01 09:35:52 +0000

    insight - medium (easy)

---
+++ partitioning_into_minimum_number_of_deci-binary_numbers_1689.py
@@ -0,0 +1,5 @@
+#https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/?envType=daily-question&envId=2026-03-01  
+
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
